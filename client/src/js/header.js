fcbedce78f8d21c6794d6c9e9a8365953db3574a
Kyle Jocoy <kjocoy@gmail.com>
2023-10-24 21:12:52 -0700

    got starter code running, added scripts, added webpack and workbox

---
+++ client/src/js/header.js
@@ -0,0 +1,12 @@
+const header = `
+/*
+       _____  ____________                      ○
+      / /   |/_  __/ ____/                 /\\__\\__/\\
+ __  / / /| | / / / __/                   /          \\
+/ /_/ / ___ |/ / / /___                \\( ﾐ  ⌒ ● ⌒ ﾐ)/
+ ____/_/  |_/_/ /_____/                GREAT GOOGLY MOOGLEY
+just another text editor              IT'S ALL GONE TO SHIT!
+*/                          
+`;
+
+export { header };
